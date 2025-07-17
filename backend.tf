0c823ce8162fa75b50aa623a3890a8ff59be9b68
DevCloudy-max <jayprajapati491@gmail.com>
2025-07-17 18:08:12 +0530

    stages-enhancment

--- backend.tf
+++ backend.tf
@@ -2,7 +2,7 @@ terraform {
   backend "s3" {
     bucket  = "techeazy-terraform-state" # ✅ Replace with your shared bucket
     key     = "terraform.tfstate"        # ⬅️ dynamic path based on stage
-    region  = "ap-south-1"               # ⬅️ Use var.region (e.g., ap-south-1)
+    region  = "ap-south-1"
     encrypt = true
   }
 }
